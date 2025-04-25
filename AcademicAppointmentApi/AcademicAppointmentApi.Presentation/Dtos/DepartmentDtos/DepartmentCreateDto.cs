90064849f52c507ceba228bf3c17ab8e34b1c8c9
Yunus Emre Maral <108576886+yunusemremaral@users.noreply.github.com>
2025-04-25 22:20:01 +0300

    Department kısmı suanlık tamam...

---
+++ AcademicAppointmentApi/AcademicAppointmentApi.Presentation/Dtos/DepartmentDtos/DepartmentCreateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AcademicAppointmentApi.Presentation.Dtos.DepartmentDtos
+{
+    public class DepartmentCreateDto
+    {
+        // Bölüm adı
+        public string Name { get; set; }
+
+        // Hangi okula ait
+        public string SchoolId { get; set; }
+    }
+}
---
+++ AcademicAppointmentApi/AcademicAppointmentApi.Presentation/Dtos/DepartmentDtos/DepartmentUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AcademicAppointmentApi.Presentation.Dtos.DepartmentDtos
+{
+    public class DepartmentUpdateDto
+    {
+        // Bölüm adı (güncelleme imkânı)
+        public string Name { get; set; }
+
+        // İsteğe bağlı: okulu değiştirmek isterseniz
+        public string SchoolId { get; set; }
+    }
+}
