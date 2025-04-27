b7420fbe87af0c60049f2322c16209c9f0412fba
Yunus Emre Maral <108576886+yunusemremaral@users.noreply.github.com>
2025-04-27 13:24:28 +0300

    Mvc layout düzenlendi . Frontend tarafına başlandı .

---
+++ AcademicAppointmentAdminMvc/AcademicAppointmentAdminMvc.MvcProject/ViewComponents/NavbarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AcademicAppointmentAdminMvc.MvcProject.ViewComponents
+{
+    public class NavbarViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ AcademicAppointmentAdminMvc/AcademicAppointmentAdminMvc.MvcProject/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AcademicAppointmentAdminMvc.MvcProject.ViewComponents
+{
+    public class SidebarViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
